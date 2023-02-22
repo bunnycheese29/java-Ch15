31e2741c7c82e7030114387cd78c22c2f36532b6
Baek Yeiwon <yeiwonxd@gmail.com>
2023-02-22 16:15:57 +0900

    server

---
+++ src/ChatServer.java
@@ -0,0 +1,90 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.*;
+import javax.swing.*;
+
+public class ChatServer extends JFrame {
+
+  private JTextField sender;
+  private Receiver receiver;
+  private ServerSocket listener = null;
+  private Socket socket = null;
+
+  private BufferedReader in = null;
+  private BufferedWriter out = null;
+
+  ChatServer() {
+    this.setTitle("채팅 서버");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Container contentPane = this.getContentPane();
+    contentPane.setLayout(new BorderLayout());
+    sender = new JTextField();
+    sender.addActionListener(
+      new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          String msg = sender.getText();
+          try {
+            out.write(msg + "\r\n");
+            out.flush();
+
+            receiver.append("\r\n 서버 : " + msg);
+            int end = receiver.getText().length();
+            receiver.setCaretPosition(end);
+            sender.setText("");
+          } catch (IOException e1) {
+            e1.printStackTrace();
+          }
+        }
+      }
+    );
+    receiver = new Receiver();
+
+    contentPane.add(sender, BorderLayout.SOUTH);
+    contentPane.add(new JScrollPane(receiver), BorderLayout.CENTER);
+    this.setSize(400, 500);
+    this.setVisible(true);
+    try {
+      listener = new ServerSocket(9998);
+      socket = listener.accept();
+      receiver.append("클라이언트 연결 완료");
+      int end = receiver.getText().length();
+      receiver.setCaretPosition(end);
+      in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      out =
+        new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    Thread thread = new Thread(receiver);
+    thread.start();
+  }
+
+  class Receiver extends JTextArea implements Runnable {
+
+    @Override
+    public void run() {
+      String msg = null;
+      while (true) {
+        try {
+          msg = in.readLine();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        this.append("\r\n 클라이언트 : " + msg);
+        int end = this.getText().length();
+        this.setCaretPosition(end);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    new ChatServer();
+  }
+}
